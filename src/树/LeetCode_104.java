5aeec3566aa41df025eeaba4a2d7abee1316d3f3
AkaShark <aaksharker@gmail.com>
2020-09-27 15:19:51 +0800

    LeetCode_104

---
+++ src/树/LeetCode_104.java
@@ -0,0 +1,51 @@
+package 树;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Queue;
+
+public class LeetCode_104 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    // 深度优先遍历
+//    public int maxDepth(TreeNode root) {
+//        if (root == null) return 0;
+//        int leftDepth = maxDepth(root.left);
+//        int rightDepth = maxDepth(root.right);
+//        return (leftDepth > rightDepth ? leftDepth : rightDepth) + 1;
+//    }
+
+    // 广度优先遍历
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+
+        int depth = 0;
+        int size = 0;
+
+        while (!queue.isEmpty()) {
+            size = queue.size();
+            while (size > 0) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                size--;
+            }
+            depth++;
+        }
+        return depth;
+    }
+}
