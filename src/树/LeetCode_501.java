64724e278b4642d57ad8d2fe04166ece1ca42e4b
AkaShark <aaksharker@gmail.com>
2020-10-07 19:51:32 +0800

    Mirrors

--- src/树/LeetCode_501.java
+++ src/树/LeetCode_501.java
@@ -36,7 +36,7 @@ public class LeetCode_501 {
         dfs(node.right);
     }
 
-    public void  update(int x) {
+    public void update(int x) {
         if (x == base) {
             count++;
         } else {
@@ -52,5 +52,35 @@ public class LeetCode_501 {
             answer.add(base);
         }
     }
+
+// Mirror 中序遍历
+    public int[] findMode_1(TreeNode root) {
+        TreeNode cur = root, pre = null;
+        while (cur != null) {
+            if (cur.left == null) {
+                update(cur.val);
+                cur = cur.right;
+                continue;
+            }
+            pre = cur.left;
+            while (pre.right != null && pre.right != cur) {
+                pre = pre.right;
+            }
+            if (pre.right == null) {
+                pre.right = cur;
+                cur = cur.left;
+            } else {
+                pre.right = null;
+                update(cur.val);
+                cur = cur.right;
+            }
+        }
+        int[] mode = new int[answer.size()];
+        for (int i = 0; i < answer.size(); ++i) {
+            mode[i] = answer.get(i);
+        }
+        return mode;
+    }
+
 
 }
