bec11d4e8ca176b801c22e5946e2d14f3825a099
AkaShark <aaksharker@gmail.com>
2020-10-14 13:23:44 +0800

    563

---
+++ src/树/LeetCode_572.java
@@ -0,0 +1,116 @@
+package 树;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LeetCode_572 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        ;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // 前序遍历 dfs 转化为t是否与s的子树相等
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        return dfs(s, t);
+    }
+
+    public boolean dfs(TreeNode s, TreeNode t) {
+        if (s == null) {
+            return false;
+        }
+        return check(s,t) || dfs(s.left, t) || dfs(s.right, t);
+    }
+
+    public boolean check(TreeNode s, TreeNode t) {
+        if (s == null && t == null) return true;
+        if (s == null || t == null || s.val != t.val) {
+            return false;
+        }
+        return check(s.left, t.left) && check(s.right, t.right);
+    }
+
+    // dfs序列 转化为s的序列是否包含t的序列 检测使用KMP算法
+    List<Integer> sOrder = new ArrayList<>();
+    List<Integer> tOrder = new ArrayList<>();
+    int maxElement, lNull, rNull;
+
+    public boolean isSubtree_1(TreeNode s, TreeNode t) {
+        maxElement = Integer.MIN_VALUE;
+        getMaxElement(s);
+        getMaxElement(t);
+        lNull = maxElement + 1;
+        rNull = maxElement + 2;
+
+        getDfsOrder(s, sOrder);
+        getDfsOrder(t, tOrder);
+
+        return kmp();
+    }
+
+    public void getMaxElement(TreeNode t) {
+        if (t == null) return;
+        maxElement = Math.max(maxElement, t.val);
+        getMaxElement(t.left);
+        getMaxElement(t.right);
+    }
+
+    public void getDfsOrder(TreeNode t, List<Integer> tar) {
+        if (t == null) return;
+        tar.add(t.val);
+        if (t.left != null) getDfsOrder(t.left, tar);
+        else tar.add(lNull);
+        if (t.right == null) getDfsOrder(t.right, tar);
+        else tar.add(rNull);
+    }
+
+    public boolean kmp() {
+        int sLen = sOrder.size(), tLen = tOrder.size();
+        int[] fail = new int[tOrder.size()];
+        Arrays.fill(fail, -1);
+        for (int i = 1, j = -1; i <tLen ; i++) {
+            while (j != -1 && !(tOrder.get(i).equals(tOrder.get(j + 1)))) {
+                j = fail[j];
+            }
+            if (tOrder.get(i).equals(tOrder.get(j + 1))) {
+                ++j;
+            }
+            fail[i] = j;
+        }
+
+        for (int i = 0, j = -1; i < sLen; ++i) {
+            while (j != -1 && !(sOrder.get(i).equals(tOrder.get(j + 1)))) {
+                j = fail[j];
+            }
+            if (sOrder.get(i).equals(tOrder.get(j + 1))) {
+                ++j;
+            }
+            if (j == tLen - 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+}
+
+
