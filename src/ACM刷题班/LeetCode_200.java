78095683dca932231867c7a8ce40b025c92cacb8
刘述豪 <aaksharker@gmail.com>
2020-11-12 15:16:12 +0800

    LeetCode_200 LeetCode_922

--- src/ACM刷题班/LeetCode_200.java
+++ src/ACM刷题班/LeetCode_200.java
@@ -1,4 +1,88 @@
 package ACM刷题班;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class LeetCode_200 {
+
+    void dfs(char[][] grid, int r, int c) {
+        int nr = grid.length;
+        int nc = grid[0].length;
+
+        // 递归出口
+        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+            return;
+        }
+        grid[r][c] = '0'; // 将这个点改变标记为 '0'
+        dfs(grid, r - 1, c); // 这个节点的上
+        dfs(grid, r + 1, c); // 下
+        dfs(grid, r, c - 1); // 左
+        dfs(grid, r, c + 1); // 右
+    }
+
+    public int numIslands_1(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int nr = grid.length; // 行数
+        int nc = grid[0].length; // 列数
+        int num_islands = 0;
+        for (int r = 0; r < nr; r++) {
+            for (int c = 0; c < nc; c++) {
+                if (grid[r][c] == '1') {
+                    num_islands++;
+                    dfs(grid, r, c);
+                }
+            }
+        }
+        return num_islands;
+    }
+
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+
+        for (int r = 0; r < nr; r++) {
+            for (int c = 0; c < nc; c++) {
+                if (grid[r][c] == '1') {
+                    ++num_islands;
+                    grid[r][c] = '0';
+                    Queue<Integer> neighbors = new LinkedList<>();
+                    neighbors.add(r*nc + c); // 位置
+                    while (!neighbors.isEmpty()) {
+                        int id = neighbors.remove();
+                        int row = id / nc;
+                        int col = id % nc;
+                        // 上
+                        if (row - 1 >= 0 && grid[row-1][col] == '1') {
+                            neighbors.add((row-1) * nc + col);
+                            grid[row-1][col] = '0';
+                        }
+                        // 下
+                        if (row + 1 < nr && grid[row+1][col] == '1') {
+                            neighbors.add((row+1) * nc + col);
+                            grid[row+1][col] = '0';
+                        }
+                        // 左
+                        if (col - 1 >= 0 && grid[row][col-1] == '1') {
+                            neighbors.add(row * nc + col-1);
+                            grid[row][col-1] = '0';
+                        }
+                        // 右
+                        if (col + 1 < nc && grid[row][col+1] == '1') {
+                            neighbors.add(row * nc + col+1);
+                            grid[row][col+1] = '0';
+                        }
+                    }
+                }
+            }
+        }
+        return num_islands;
+    }
+
 }
