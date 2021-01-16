2822acd36f2fb5a199ffc3d12077881d92a740c2
Sharker <1548742234@qq.com>
2021-01-17 00:11:13 +0800

    offer_42

--- src/剑指offer/offer_42.java
+++ src/剑指offer/offer_42.java
@@ -7,7 +7,7 @@ public class offer_42 {
         if (nums.length == 1) return nums[0];
         int max = Integer.MIN_VALUE;
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i; j < nums.length; j++) {
+            for (int j = i; j <= nums.length; j++) {
                 // 计算i到j的和
                 int sum = 0;
                 for (int k = i; k < j; k++) {
@@ -18,4 +18,33 @@ public class offer_42 {
         }
         return max;
     }
+    // n2 时间复杂度过高
+    public int maxSubArray_1(int[] nums) {
+        if (nums == null || nums.length == 0) return -1;
+        if (nums.length == 1) return nums[0];
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            for (int j = i; j < nums.length; j++) {
+                // 计算i到j的和 以及循环了i 在 循环的过程中加上j
+               sum += nums[j];
+            }
+            if (sum > max) max = sum;
+        }
+        return max;
+    }
+
+    public int maxSubArray_2(int[] nums) {
+        if (nums == null || nums.length == 0) return -1;
+        if (nums.length == 1) return nums[0];
+        int res = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] += Math.max(nums[i - 1], 0);
+            res = Math.max(res, nums[i]);
+        }
+        return res;
+    }
+
+
+
 }
