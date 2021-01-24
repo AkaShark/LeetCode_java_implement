27a93a584d3a879abf93209766eae5f7bf371b2a
plys Cui <204529929@qq.com>
2021-01-24 17:43:31 +0800

    offer_49 丑数

--- src/剑指offer/offer_49.java
+++ src/剑指offer/offer_49.java
@@ -4,28 +4,20 @@ public class offer_49 {
     // 我们把只包含质因子 2、3 和 5 的数称作丑数（Ugly Number）。求按从小到大的顺序的第 n 个丑数
     // 暴力法解决判断0-n的每个数是否是丑数
     public int nthUglyNumber(int n) {
-        if (n <= 0) return -1;
-        if (n == 1 || n == 2 || n == 3) return n;
-        int index = 0, res = 0;
-        for (int i = 3; n < 0; i++) {
-
-            while (i % 2 == 0 && i > 0) {
-                i /= 2;
-                while (i % 3 == 0 && i > 0) {
-                    i /= 3;
-                    while (i % 5 == 0 && i > 0) {
-                        i /= 5;
-                    }
-                }
-            }
-
-            if (i == 0) {
-                n --;
-            }
-            if (n == 1) res = i;
+        int a = 0, b = 0, c = 0;
+        int[] dp = new int[n]; // 使用dp数组来存储丑数序列
+        dp[0] = 1; // 1为丑数 默认为dp的第0个元素
+        for (int i = 1; i < n; i++) { // 循环n-1次
+            // 获取下一个丑数的可能（*2 *3 *5）
+            int n2 = dp[a] * 2, n3 = dp[b] * 3, n5 = dp[c] * 5;
+            // 获取第i+1个丑数  是所有可能的最小值 因为要保证生成丑数的顺序
+            dp[i] = Math.min(Math.min(n2, n3), n5);
+            // 判断新生成的丑数是哪个可能产生的 =》 这个个a/b/c 完成了使命下个要成的数是a/b/c+1
+            if (dp[i] == n2) a++;
+            if (dp[i] == n3) b++;
+            if (dp[i] == n5) c++;
         }
-
-         return res;
+        return dp[n - 1]; // dp[i] 表示第i+1位丑数 则第n位为 dp[n - 1]
 
     }
 }
