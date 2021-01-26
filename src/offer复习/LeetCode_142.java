c061b8fc05c42249b9daba74cb50f2dd5406db24
plys Cui <204529929@qq.com>
2021-01-27 00:11:49 +0800

    offer_52

---
+++ src/offer复习/LeetCode_142.java
@@ -0,0 +1,61 @@
+package offer复习;
+
+public class LeetCode_142 {
+/*
+* 给定一个链表，返回链表开始入环的第一个节点。如果链表无环，则返回null。
+  为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。
+* 如果 pos 是 -1，则在该链表中没有环
+*/
+    public class ListNode {
+        int val;
+        ListNode next;
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+    public ListNode detectCycle_1(ListNode head) {
+        if (head == null) return null;
+        ListNode fast = head, slow = head;
+        while (fast != null) {
+            slow = slow.next;
+            if (fast.next != null) fast = fast.next.next;
+            else return null;
+            if (slow == fast) {
+                fast = head;
+                while (fast != slow) {
+                    fast = fast.next;
+                    slow = slow.next;
+                }
+                return slow;
+            }
+
+        }
+        return null;
+    }
+
+    // leetCode
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) return null;
+        ListNode slow = head, fast = head;
+        while (fast != null) {
+            slow = slow.next;
+            if (fast.next != null) {
+                fast = fast.next.next;
+            } else return null;
+            if (fast == slow) {
+                ListNode ptr = head;
+                while (ptr != slow) {
+                    ptr = ptr.next;
+                    slow = slow.next;
+                }
+                return ptr;
+            }
+        }
+        return null;
+    }
+
+
+}
---
+++ src/剑指offer/offer_52.java
@@ -0,0 +1,2 @@
+package 剑指offer;public class offer_52 {
+}
