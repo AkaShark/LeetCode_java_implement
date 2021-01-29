d14f224b9a273d74d6dece370d752892896301c3
plys Cui <204529929@qq.com>
2021-01-30 00:08:47 +0800

    leetCode_101

--- src/offer复习/leetcode_101.java
+++ src/offer复习/leetcode_101.java
@@ -1,5 +1,32 @@
 package offer复习;
 
 public class leetcode_101 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
 
+        public TreeNode() {
+        }
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        return check(root, root);
+    }
+
+    public boolean check(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) return true;
+        if (root1 == null || root2 == null) return false;
+        return root1.val == root2.val && check(root1.left, root2.right) && check(root1.right, root2.left);
+    }
 }
