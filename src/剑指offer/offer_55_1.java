daf488cdb341a6b974d6c369198b417ddd0cdccf
plys Cui <204529929@qq.com>
2021-02-02 18:09:51 +0800

    offer_55_1 & offer_55_2 补充

--- src/剑指offer/offer_55_1.java
+++ src/剑指offer/offer_55_1.java
@@ -1,6 +1,7 @@
 package 剑指offer;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class offer_55_1 {
@@ -27,11 +28,12 @@ public class offer_55_1 {
     public int maxDepth_1(TreeNode root) {
         if (root == null) return 0;
         if (root.left == null && root.right == null) return 1;
+        // 树的深度就是左右子树深度的最大值 + 1 +1 是root这一层的深度
         return Math.max(maxDepth_1(root.left), maxDepth_1(root.right)) + 1;
     }
 
     // 非递归实现 层序遍历
-    public int maxDepth(TreeNode root) {
+    public int maxDepth_2(TreeNode root) {
         if (root == null) return 0;
         if (root.left == null && root.right == null) return 1;
         Queue<TreeNode> queue = new LinkedList<>();
@@ -49,5 +51,30 @@ public class offer_55_1 {
         }
         return res;
     }
+
+    // leetCode 非递归实现
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        // 使用LinkedList模拟队列
+        List<TreeNode> queue = new LinkedList<TreeNode>(){{add(root);}}, tmp;
+        int res = 0;
+        while (!queue.isEmpty()) {
+            // 创建临时队列添加下一层的节点
+            tmp = new LinkedList<>();
+            // 遍历这层的节点
+            for (TreeNode node : queue) {
+                if (node.left != null) tmp.add(node.left);
+                if (node.right != null) tmp.add(node.right);
+            }
+            // 将下层节点赋值给queue
+            queue = tmp;
+            res ++; // 标记器++ 完成一层的遍历标签器++
+            // 我在处理这个地方的思路差不多我是使用了一个int size标识 来记录当前queue的size 同时这个size就是
+            // 上层节点的个数 遍历完这层节点后 标签器++  遍历结束的标志是循环size次
+            // 同时在遍历的时候将下层节点添加到queue中相当于没有使用临时的队列
+        }
+        return res;
+    }
+
 
 }
--- src/剑指offer/offer_55_2.java
+++ src/剑指offer/offer_55_2.java
@@ -13,24 +13,57 @@ public class offer_55_2 {
     /*
     * 输入一棵二叉树的根节点，判断该树是不是平衡二叉树。
     * 如果某二叉树中任意节点的左右子树的深度相差不超过1，那么它就是一棵平衡二叉树。*/
-    // 左子树右子树高度差 中序遍历
-    public boolean isBalanced(TreeNode root) {
+    // 左子树右子树高度差 先序遍历(这个只需要遍历树就可以了 不一定是先序遍历)
+    boolean flag = true;
+    public boolean isBalanced_1(TreeNode root) {
         if (root == null) return true;
-
         int leftH = 0, rightH = 0;
-        leftH = TreeHeight(root.left);
-        rightH = TreeHeight(root.right);
-        if (Math.abs(leftH - rightH) > 1) {
-            return false;
+        leftH = TreeHeight(root.left); // 左子树的高度
+        rightH = TreeHeight(root.right); // 右子树的高度
+        if (Math.abs(leftH - rightH) > 1) { // 高度差是否超过1
+            flag = false;
         }
-        isBalanced(root.left);
-        isBalanced(root.right);
-        return true;
+        isBalanced_1(root.left); // 遍历左子树
+        isBalanced_1(root.right); // 遍历右子树
+        return flag;
     }
-
+    // 获取子树的高度
     public int TreeHeight(TreeNode root) {
         if (root == null) return 0;
         if (root.left == null && root.right == null) return 1;
         return Math.max(TreeHeight(root.left), TreeHeight(root.right)) + 1;
     }
+
+    // LeetCode
+
+    // 后序遍历 + 剪枝 (从低至顶)
+    public boolean isBalanced_2(TreeNode root) {
+        // 最后返回的是树的深度
+        return recur(root) != -1; // 返回当recur不为 -1的时候返回返回true 否则返回false
+    }
+    // 后序遍历
+    public int recur(TreeNode root) {
+        if (root == null) return 0;
+        int left = recur(root.left);
+        if (left == -1) return  -1; // 剪枝操作 如果高度返回为-1 直接返回上一层
+        int right = recur(root.right);
+        if (right == -1) return -1;
+        // 如果不满足平衡二叉树的条件的话返回 -1 如果满足的话返回当前树的深度 一层一层的往上返回根节点
+        return Math.abs(left - right) < 2 ? Math.max(left, right) + 1: -1;
+    }
+
+    // (遍历树)先序遍历 + 判断深度 (从顶到底 但是会产生重复计算)
+    public  boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        return Math.abs(depth(root.left) - depth(root.right)) < 2 &&
+                isBalanced(root.left) &&
+                isBalanced(root.right);
+    }
+    private int depth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(depth(root.left), depth(root.right)) + 1;
+    }
+
+
+
 }
