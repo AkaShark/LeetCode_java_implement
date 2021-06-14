2f90e73d3a3151a0089fde3f54f6aea7dceee300
liushuhao <liushuhao@coachview.com.cn>
2021-06-14 14:58:00 +0800

    Leetcode 17 电话号码的字母组合 回溯问题联系

--- src/Hot100/LeetCode_17.java
+++ src/Hot100/LeetCode_17.java
@@ -3,6 +3,7 @@ package Hot100;
 import java.util.*;
 
 public class LeetCode_17 {
+    // 对照的映射
     Map<Character, String> phone = new HashMap<>();
     phone.put('2',"abc");
     phone.put('3',"def");
@@ -27,7 +28,7 @@ public class LeetCode_17 {
         return ans;
     }
 
-    // 关键代码 回溯法
+    // 关键代码 回溯法  组合求全排列
     public List<String> combination(List<String> inputList) {
         List<String> resList = new ArrayList<>();
         combinationInt(inputList, resList, 0,
@@ -41,12 +42,14 @@ public class LeetCode_17 {
             resList.add(new String(arr));
             return;
         }
-        // 将String转成CharArray
+        // 将String转成CharArray ["abc", "def"] => [a, b, c]
         for (char c : inputList.get(ind).toCharArray()) {
             arr[ind] = c;
+            // 递归的整合
             combinationInt(inputList, resList, ind + 1, arr);
         }
     }
+
 
 
     public List<String> letterCombinations(String digits) {
@@ -65,17 +68,17 @@ public class LeetCode_17 {
             int lettersCount = letters.length();
             for (int i = 0; i < lettersCount; i++) {
                 combination.append(letters.charAt(i));
+                // 回溯 与上一个方法的不同的地方在于上一个使用char[] 来放对应的生成好的子字符串 但是这个方法使用StringBuffer来装 上一个依托于index 这个直接使用相对灵活的StringBuffer
                 backtrack(combinations, digits, index + 1, combination);
                 combination.deleteCharAt(index);
             }
-
         }
     }
 
     // 队列实现 最终的queue就是结果
     public List<String> letterCombinations_2(String digits) {
         Queue<String> queue = new LinkedList<String>();
-        List<String> phone=new ArrayList<>();
+        List<String> phone = new ArrayList<>();
         if(digits.length()==0) return new ArrayList<String>();
         phone.add("abc");
         phone.add("def");
