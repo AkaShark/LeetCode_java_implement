7a2e740c3a052e73be3b59769d5bed3fab341ab0
Sharker <1548742234@qq.com>
2021-10-14 15:47:34 +0800

    feat: LeetCode_344

--- src/Hot100/LeetCode_344.java
+++ src/Hot100/LeetCode_344.java
@@ -1,2 +1,13 @@
-package Hot100;public class LeetCode_344 {
+package Hot100;
+
+public class LeetCode_344 {
+    public void reverseString(char[] s) {
+        int left = 0, right = s.length - 1;
+        while (right >= left) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+        }
+        return;
+    }
 }
